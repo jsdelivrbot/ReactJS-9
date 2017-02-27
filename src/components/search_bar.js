a6876ef44e5ac1fb37985e4fc5e927db09938c78
unravel-the-sky <sekdemir@gmail.com>
2017-02-27 23:11:09 +0100

    update; WOW. go react! now managed to fetch videos from the youtube API and play a video. important thing is functional objects and class objects! class objects hold states, which we pass on to the functional objects as props. awesome;

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) { // all JS classes have the constructor method 
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input onChange={(event) => console.log(event.target.value)} />; // bu event handler, onChange de React event, onu this.onInputChange ile override ediyoruz
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={(event) => this.setState({ term: event.target.value })} />
+            </div>
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCf4JsMgCpiofErRsns3Hg9_QhcDXhHZn4';
 
-// create component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'westworld' }, (videos) => {
+            this.setState({ videos }); // this is the same as saying videos : videos (es6 syntax)
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // take it and put it into DOM
